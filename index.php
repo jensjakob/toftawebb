c67ac33b71a18312883c344370ef6d58f284e6ec
jensjakob <jensjakob@users.noreply.github.com>
2018-05-11 09:29:39 +0200

    first step to be a theme

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Libre+Franklin:400" rel="stylesheet">
+	<?php wp_head() ?>
+</head>
+<body>
+
+	<div id="navbar">
+		<div id="logo"><a href="/"><img width="200" src="http://toftawebb.se/files/2017/11/cropped-logo_tofta_webbyra_white.png" alt="🚐 Tofta Webbyrå" /></a></div>
+		<div id="show-menu"><a href="#" onClick="toggleMenu()"><img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjUiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNSAyNCIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj4KPGcgZmlsbD0ibm9uZSI+CjxwYXRoIGQ9Ik0gMCAwTCAyNSAwIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDIxKSIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSI1Ii8+CjxwYXRoIGQ9Ik0gMCAwTCAyNSAwIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDEyKSIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSI1Ii8+CjxwYXRoIGQ9Ik0gMCAwTCAyNSAwIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDMpIiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjUiLz4KPC9nPgo8L3N2Zz4K" /></a></div>
+	</div>
+
+	<div id="nav-items">
+		<ul>
+			<li><a href="#">Presentation</a></li>
+			<li><a href="#">Tjänster</a></li>
+			<li><a href="#">Kunder</a></li>
+			<li><a href="#">Kontaktuppgifter</a></li>
+		</ul>
+	</div>
+	
+	<div id="content">
+
+		
+		<div id="cta-top">
+			<div id="top-image">
+			</div>
+
+			<div id="cta-container">
+			<div id="cta-copy">
+				<h1>Städa din webbplats<br>och öka försäljningen</h1>
+				<p><strong>Jag är van att komma in i ett kaos av idéer och halvklara tekniska lösningar. Då är det värt att vara erfaren agil projektledare och certifierad Scrum Master.</strong></p>
+				<p>Snabbt hittar vi affärsnyttan och fixar de viktigaste sakerna först.</p>
+				<p>
+					<a href="https://toftawebb.drift.com/jakob?schedule" class="button check">Boka möte</a> <a href="#">Mer info...</a>
+				</p>
+
+			</div>
+			</div>
+		</div>
+
+
+		<iframe width="100%" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBPPMOQVTT8zHWbgtRkfZLOAZzzYoA7Wb4&q=Tofta%20Webbyrå&zoom=9" allowfullscreen></iframe>
+
+		<div class="boxes">
+			<div class="infobox"><h2><a href="#">Presentation</a></h2>
+			<p>Jag har 20 års erfarenhet av att bygga webb. Med en bakgrund som webbutvecklare blir det så mycket enklare att ge förslag på bra lösningar och prioriteringar. Välkommen att kontakta Gotlands nya miljövänliga webbbyrå.</p>
+			<p>➽ <a href="#">Vad jag gjort och min bakgrund...</a></p></div>
+
+			<div class="infobox"><h2><a href="#">Tjänster</a></h2>
+			<p>Det ska inte bara se bra ut. Det ska också finnas struktur så man hittar det man letar efter, framför allt i mobiltelefonen. UX och interaktionsdesign är extremt viktigt för att öka försäljningen. Där finns det massor med jobb att göra!</p>
+			<p>➽ <a href="#">Vad jag kan hjälpa dig med...</a></p></div>
+
+			<div class="infobox"><h2><a href="#">Kunder</a></h2>
+			<p>Du kanske känner till Pickit, eller Anders Arborist här på Gotland. Men jag har också jobbat för stora kunder som Bohmans Nätverk, Svenska Spel, Scandic, Internetworld samt varit anställd på Spotify, Middagsfrid och Tradedoubler.</p>	
+			<p>➽ <a href="#">Vad andra tycker om mig...</a></p></div>
+		</div>
+
+	</div>
+	
+	<footer>
+			
+		<img class="by-me" src="https://jensplan.se/toftawebb/files/2017/10/jakob-150x150.jpg" alt="" width="150" height="150" />
+			
+		<div class="footer-text">
+			<p><strong>Tofta Webbyrå</strong><br>
+			Jakob Nanneson</p>
+			
+			<p><a href="mailto:info@toftawebb.se">info@toftawebb.se</a><br>
+			tel. 075-760 17 00</p>
+			
+			<p>➽ <strong><a href="https://toftawebb.drift.com/jakob?schedule">Boka möte</a></strong></p>
+		</div>
+	</footer>
+	<?php wp_footer() ?>
+</body>
+</html>
