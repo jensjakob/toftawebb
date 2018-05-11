a7eea58de7723af30ecb7800976883051d89d628
jensjakob <jensjakob@users.noreply.github.com>
2018-05-11 10:27:10 +0200

    using dynamic content

--- index.php
+++ index.php
@@ -9,61 +9,75 @@
 <body>
 
 	<div id="navbar">
-		<div id="logo"><a href="/"><img width="200" src="http://toftawebb.se/files/2017/11/cropped-logo_tofta_webbyra_white.png" alt="🚐 Tofta Webbyrå" /></a></div>
+		<div id="logo"><a href="/"><img width="200" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/logo_tofta_webbyra_white.png" alt="🚐 Tofta Webbyrå" /></a></div>
 		<div id="show-menu"><a href="#" onClick="toggleMenu()"><img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjUiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNSAyNCIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj4KPGcgZmlsbD0ibm9uZSI+CjxwYXRoIGQ9Ik0gMCAwTCAyNSAwIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDIxKSIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSI1Ii8+CjxwYXRoIGQ9Ik0gMCAwTCAyNSAwIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDEyKSIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSI1Ii8+CjxwYXRoIGQ9Ik0gMCAwTCAyNSAwIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgwIDMpIiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjUiLz4KPC9nPgo8L3N2Zz4K" /></a></div>
 	</div>
 
 	<div id="nav-items">
-		<ul>
-			<li><a href="#">Presentation</a></li>
-			<li><a href="#">Tjänster</a></li>
-			<li><a href="#">Kunder</a></li>
-			<li><a href="#">Kontaktuppgifter</a></li>
-		</ul>
+		<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
 	</div>
 	
 	<div id="content">
-
 		
-		<div id="cta-top">
-			<div id="top-image">
-			</div>
+		<?php if (is_front_page()): ?>
 
+		<div id="cta-top">
+			<style>
+			#top-image {
+				background-image: url('<?php the_post_thumbnail_url(); ?>');
+			}
+			</style>
+			<div id="top-image"></div>
 			<div id="cta-container">
 			<div id="cta-copy">
-				<h1>Städa din webbplats<br>och öka försäljningen</h1>
-				<p><strong>Jag är van att komma in i ett kaos av idéer och halvklara tekniska lösningar. Då är det värt att vara erfaren agil projektledare och certifierad Scrum Master.</strong></p>
-				<p>Snabbt hittar vi affärsnyttan och fixar de viktigaste sakerna först.</p>
-				<p>
-					<a href="https://toftawebb.drift.com/jakob?schedule" class="button check">Boka möte</a> <a href="#">Mer info...</a>
-				</p>
-
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; endif; ?>
 			</div>
 			</div>
 		</div>
-
-
 		<iframe width="100%" height="350" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBPPMOQVTT8zHWbgtRkfZLOAZzzYoA7Wb4&q=Tofta%20Webbyrå&zoom=9" allowfullscreen></iframe>
+		<?php else: ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            <?php endwhile; endif; ?>
+		<?php endif; ?>
+
 
 		<div class="boxes">
-			<div class="infobox"><h2><a href="#">Presentation</a></h2>
-			<p>Jag har 20 års erfarenhet av att bygga webb. Med en bakgrund som webbutvecklare blir det så mycket enklare att ge förslag på bra lösningar och prioriteringar. Välkommen att kontakta Gotlands nya miljövänliga webbbyrå.</p>
-			<p>➽ <a href="#">Vad jag gjort och min bakgrund...</a></p></div>
+		
+        <?php 
+        $args = array(
+            'post_type' => 'page',
+            'orderby' => 'menu_order',
+			'order' => 'asc',
+			'post_parent' => 0,
+			'posts_per_page' => 3,
+			'offset' => 1
+        );
+        
+        $the_query = new WP_Query( $args );
+        ?>
+        <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+		
+			<div class="infobox">
+				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?> &rarr;</a></h2>
+				<p><?php the_excerpt(); ?></p>
+			</div>
 
-			<div class="infobox"><h2><a href="#">Tjänster</a></h2>
-			<p>Det ska inte bara se bra ut. Det ska också finnas struktur så man hittar det man letar efter, framför allt i mobiltelefonen. UX och interaktionsdesign är extremt viktigt för att öka försäljningen. Där finns det massor med jobb att göra!</p>
-			<p>➽ <a href="#">Vad jag kan hjälpa dig med...</a></p></div>
-
-			<div class="infobox"><h2><a href="#">Kunder</a></h2>
-			<p>Du kanske känner till Pickit, eller Anders Arborist här på Gotland. Men jag har också jobbat för stora kunder som Bohmans Nätverk, Svenska Spel, Scandic, Internetworld samt varit anställd på Spotify, Middagsfrid och Tradedoubler.</p>	
-			<p>➽ <a href="#">Vad andra tycker om mig...</a></p></div>
+        <?php
+        endwhile;
+        wp_reset_postdata();
+		?>
+		
 		</div>
 
 	</div>
 	
 	<footer>
 			
-		<img class="by-me" src="https://jensplan.se/toftawebb/files/2017/10/jakob-150x150.jpg" alt="" width="150" height="150" />
+		<img class="by-me" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/jakob.jpg" alt="" width="150" height="150" />
 			
 		<div class="footer-text">
 			<p><strong>Tofta Webbyrå</strong><br>
